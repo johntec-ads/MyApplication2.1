910404dbbed487be7faa92a140b934c893db97ca
johntec-ads <johntec.ads@gmail.com>
2022-03-15 06:23:05 -0300

    Atualizando repositorio

---
+++ app/src/main/java/com/example/myapplication20/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication20
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btLogin).setOnClickListener{
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener {
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        }
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener {
+            startActivity(Intent(this,CadastroActivity::class.java))
+        }
+    }
+}
