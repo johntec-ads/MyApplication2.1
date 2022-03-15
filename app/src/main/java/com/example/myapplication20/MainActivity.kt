2567377c1b0697582ffd520984451b6df9304f82
johntec-ads <johntec.ads@gmail.com>
2022-03-15 06:27:25 -0300

    Respondendo a erro de unpush.

--- app/src/main/java/com/example/myapplication20/MainActivity.kt
+++ app/src/main/java/com/example/myapplication20/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication20
 //Teste de unpoush.
+//responde a erro de unpush
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
